ae4626930a43fc537e9ceaa511334ddf254c8872
Your GitHub Username <Your GitHub Email>
2024-08-28 03:40:55 -0600

    Added changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,12 @@ export default function RootLayout({
   children: React.ReactNode,
 }) {
   return (
-    <html lang='en'>
-     
+    <html lang="en">
+      <meta
+        name="google-site-verification"
+        content="T8Co76v3XWFDCVzAR0x6lWNzYrtIx_ZerdNX-2emX9U"
+      />
+
       <body>
         <Providers>{children}</Providers>
       </body>
