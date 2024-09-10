7c3555cbe25873adc99c9e4532cc2aefa9681497
Your GitHub Username <Your GitHub Email>
2024-09-10 06:25:44 -0600

    Added changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import { Providers } from "./Provider";
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -9,13 +10,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta
-        name="google-site-verification"
-        content="T8Co76v3XWFDCVzAR0x6lWNzYrtIx_ZerdNX-2emX9U"
-      />
-
+      <Head>
+        <meta name="google-site-verification" content="T8Co76v3XWFDCVzAR0x6lWNzYrtIx_ZerdNX-2emX9U" />
+      </Head>
       <body>
         <Providers>{children}</Providers>
+
+        {/* Microsoft Clarity Script */}
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){ 
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; 
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; 
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); 
+            })(window, document, "clarity", "script", "o0enwfipb4");`,
+          }}
+        />
       </body>
     </html>
   );
